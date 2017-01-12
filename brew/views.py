15cf47efce1614413473f8996647b3d70ac86e91
AriiMoose <ariimoose@gmail.com>
2017-01-12 17:09:29 +0000

    Added remaining custom error pages

--- brew/views.py
+++ brew/views.py
@@ -56,6 +56,26 @@ general_style = Style(
   	)
 
 
+def bad_request(request):
+	response = render_to_response(
+		'brew/500.html',
+		context_instance=RequestContext(request)
+		)
+
+	response.status_code = 400
+
+	return response
+
+def permission_denied(request):
+	response = render_to_response(
+		'brew/500.html',
+		context_instance=RequestContext(request)
+		)
+
+	response.status_code = 403
+
+	return response
+
 def page_not_found(request):
 	response = render_to_response(
 		'brew/404.html',
